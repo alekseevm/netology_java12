035b65cc6d3c3de7a83e6fb0be4d2c72ff3501b5
alekseev <alekseev.m@me.com>
2022-08-22 11:11:06 +0300

    update

--- src/test/java/Netology_java12/AfishaTest.java
+++ src/test/java/Netology_java12/AfishaTest.java
@@ -74,4 +74,23 @@ public class AfishaTest {
         String[] actual = {"movie 1", "movie 2", "movie 3", "movie 4", "movie 5", "movie 6", "movie 7", "movie 8", "movie 9", "movie 10"};
         Assertions.assertArrayEquals(expect, actual);
     }
+
+    @Test
+    public void findAllIfQtyLessLimit(){
+        Afisha afisha = new Afisha(14);
+        afisha.add("movie 1");
+        afisha.add("movie 2");
+        afisha.add("movie 3");
+        afisha.add("movie 4");
+        afisha.add("movie 5");
+        afisha.add("movie 6");
+        afisha.add("movie 7");
+        afisha.add("movie 8");
+        afisha.add("movie 9");
+        afisha.add("movie 10");
+
+        String[] expect = afisha.findLast();
+        String[] actual = {"movie 10", "movie 9", "movie 8", "movie 7", "movie 6", "movie 5", "movie 4", "movie 3", "movie 2", "movie 1"};
+        Assertions.assertArrayEquals(expect, actual);
+    }
 }
